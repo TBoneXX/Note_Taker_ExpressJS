84c8f02f8f95b7c49568c5121aee926ac630fb4d
Mark Turner <mark.edward.turner@gmail.com>
2022-12-01 09:40:33 -0500

    Added Middleware, API and HTML routes

--- server.js
+++ server.js
@@ -7,4 +7,35 @@ const path = require('path');
 
 const notes = require('./db/db.json');
 
+const uuid = require('uuid');
 
+
+//Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//STATIC Middleware
+app.use(express.static('public'));
+
+//HTML Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//API Routes
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
+
+app.post('api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid();
+    notes.push(newNote);
+    fs.writeFileSync('./dp/db.json', JSON.stringify);
+    res.json(notes);
+
+})
